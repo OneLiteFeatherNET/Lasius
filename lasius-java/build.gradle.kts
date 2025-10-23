c9d40bf32e0319ca4b614d3f229a50bbc4756c61
theEvilReaper <theEvilReaper@users.noreply.github.com>
2025-10-23 19:20:08 +0200

    feat(plugin): add missing plugin definition

--- lasius-java/build.gradle.kts
+++ lasius-java/build.gradle.kts
@@ -5,6 +5,12 @@ plugins {
 group = "net.onelitefeather"
 version = "1.0-SNAPSHOT"
 
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(25))
+    }
+}
+
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
@@ -13,4 +19,15 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+gradlePlugin {
+    plugins {
+        register("lasiusJavaPlugin") {
+            id = "net.onelitefeather.lasius-java"
+            implementationClass = "net.onelitefeather.lasius.LasiusJavaPlugin"
+            displayName = "Lasius Java Plugin"
+            description = "A gradle plugin to apply default settings to java projects"
+        }
+    }
 }
