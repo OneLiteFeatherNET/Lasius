0fce87e4c45d6145840d3723a38eb07844dc0a3b
theEvilReaper <theEvilReaper@users.noreply.github.com>
2025-10-23 19:52:30 +0200

    feat(publishing): add publishing configuration for each module

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,4 +10,29 @@ subprojects {
             }
         }
     }
+
+    plugins.withType<MavenPublishPlugin> {
+        project.extensions.configure<PublishingExtension> {
+
+            publications.create<MavenPublication>("maven") {
+                from(components["java"])
+            }
+
+            repositories {
+                maven {
+                    name = "OneLiteFeatherRepository"
+                    url = if (project.version.toString().contains("SNAPSHOT")) {
+                        uri("https://repo.onelitefeather.dev/onelitefeather-snapshots")
+                    } else {
+                        uri("https://repo.onelitefeather.dev/onelitefeather-releases")
+                    }
+
+                    credentials(PasswordCredentials::class) {
+                        username = System.getenv("ONELITEFEATHER_MAVEN_USERNAME")
+                        password = System.getenv("ONELITEFEATHER_MAVEN_PASSWORD")
+                    }
+                }
+            }
+        }
+    }
 }
